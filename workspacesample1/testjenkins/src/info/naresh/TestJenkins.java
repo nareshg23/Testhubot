09177a2cd54d3ef8472ac9e96999d83f0eb11d5c
naresh <nareshg@Newt-Pk023.newtglobal.chn.local>
2016-04-18 15:56:18 +0530

    testjenkins commit

---
+++ workspacesample1/testjenkins/src/info/naresh/TestJenkins.java
@@ -0,0 +1,11 @@
+package info.naresh;
+
+public class TestJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("from TestJenkins");
+	}
+
+}
