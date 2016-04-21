6283abb5caad90235f6325f81213fca8f5ad511d
naresh <nareshg@Newt-Pk023.newtglobal.chn.local>
2016-04-21 15:53:34 +0530

    testhubot commit

---
+++ workspacesample1/testhubot/src/testhubot/Test.java
@@ -0,0 +1,11 @@
+package testhubot;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("from demo testhubot project Hello");
+
+	}
+
+}
